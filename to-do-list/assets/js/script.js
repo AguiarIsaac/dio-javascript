379b4458b9a80b7906be0b83d041d1fe8232eb7b
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-04-22 12:22:36 -0300

    Add files via upload

---
+++ to-do-list/assets/js/script.js
@@ -0,0 +1,7 @@
+let inputText = document.getElementById('to-do')
+let button = document.getElementById('btn')
+let list = document.getElementById('list')
+
+button.addEventListener('click', () => {
+    list.innerHTML += `<li><input type="checkbox" id="check_${inputText.value}"><label for="check_${inputText.value}"> ${inputText.value}</label>;</li>`
+})
