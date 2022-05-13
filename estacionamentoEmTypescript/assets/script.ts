1e4bc4621b3bf9bc95cd35df0389180e3b6122f9
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-05-13 16:06:42 -0300

    Update script.ts

--- estacionamentoEmTypescript/assets/script.ts
+++ estacionamentoEmTypescript/assets/script.ts
@@ -1,6 +1,7 @@
 interface Vehicle {
     name: string,
     plate: string,
+    color: string,
     date: Date
 }
 
@@ -9,46 +10,65 @@ interface Vehicle {
 
 
      function patio(){
-        function wride() {
 
-        }
-
-        function add(vehicle: Vehicle) {
+        function add(vehicle: Vehicle, saveCar?: Boolean) {
             const row = document.createElement('tr');
 
             row.innerHTML = `
             <td>${vehicle.name}</td>
+            <td>${vehicle.color}</td>
             <td>${vehicle.plate}</td>
             <td>${vehicle.date}</td>
-            <td><button class="dele" data-placa="${vehicle.plate}">X</button></td>
-        `;
+            <td><button class="delete" data-plate="${vehicle.plate}"><img src="/assets/svg/icon_delete.svg" alt="icone de lixeira"></button></td>
+        `;  
+
+            row.querySelector('.delete')?.addEventListener('click', function() {
+                remove(this.dataset.plate)
+            })
+            
             htmlElement('#patio')?.appendChild(row)
+
+            if (saveCar) save([...read(), vehicle])
+        }
+        
+        function save(vehicles: Vehicle[]) {
+            localStorage.setItem('patio', JSON.stringify(vehicles))
         }
 
-        function remove() {
-
-        }
-
-        function save() {
-
+        function read() : Vehicle[] {
+            return localStorage.patio ? JSON.parse(localStorage.patio) : []
         }
 
         function render(){
+            htmlElement('#patio')!.innerHTML = ''
+            const patio = read()
 
+            if (patio.length) {
+                patio.forEach((vehicle) => add(vehicle));
+            }
         }
 
-        return {wride, add, remove, save, render}
+        function remove(plate: string) {
+            const { date, name } = read().find(vehicle => vehicle.plate === plate)
+        }
+
+        return {read, add, remove, save, render}
      }
 
      htmlElement('#btn')?.addEventListener('click', () => {
          const name = htmlElement('#name')?.value;
          const plate = htmlElement('#plate')?.value;
+         const color = htmlElement('#color')?.value;
 
-         if(!name || !plate) {
+         if(!name || !plate || !color) {
              window.alert('Todos os campos devem ser preenchidos');
              return;
          }
 
-         patio().add({ name, plate,date: new Date() })
+         patio().add({ name, plate, color, date: new Date() }, true)
+
+         //patio().render();
      })
+
+     patio().render();
 })();
